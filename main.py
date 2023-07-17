8f22c8fedd3467a982b088d110c3fa7e72412470
Raynarak <raynarakerolayne4@gmail.com>
2023-07-17 09:02:42 -0300

    Comit novo4

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import csv
 import streamlit as st
+import yfinance as yf
+import plotly.graph_objects as go
+import pandas as pd
 
+# Título do app
+st.title('Stock History App')
 
-df = pd.read_csv('prices.csv')
+# Criando o sidebar
+st.sidebar.title('Selecione o stock')
+ticker_symbol = st.sidebar.text_input('stock', 'AAPL', max_chars=10)
 
-df['date'] = pd.to_datetime(df['date'])
+# Baixando dados do yahoo finaças
+data = yf.download(ticker_symbol, start = '2020-01-01', end = '2023-06-26')
 
-df['adjusted'] = df['adjusted'].astype('float')
+# Calcular a EMA usando pandas
+window = 20  # Janela da EMA
+data['EMA'] = data['Close'].ewm(span=window, adjust=False).mean()
 
-df.plot.bar(x='adjusted', y='date')
+# Exibir os dados
+st.subheader('Histórico')
+st.dataframe(data)
 
-plt.title('Adjusted Prices')
+# Exibir o gráfico
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data.index, y=data['Close'], name = 'Fechamento'))
+fig.add_trace(go.Scatter(x=data.index, y=data['EMA'], name='EMA'))
 
-st.plt.show()
+fig.update_layout(title = f"{ticker_symbol}", xaxis_title = "Data", yaxis_title = "Preço")
+st.plotly_chart(fig)
